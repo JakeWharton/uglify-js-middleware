339368f5e85ffad67433ab5844b638d71bd623d3
Jake Wharton <jakewharton@gmail.com>
2011-05-02 19:06:23 -0400

    Initial import.

---
+++ README.md
@@ -0,0 +1,66 @@
+Uglify JS Middleware
+====================
+
+This library is an extension for the [Uglify JS][1] library to add middleware
+for simple use with the [Connect][2] library to automatically uglify your JS
+whenever a source file has been updated.
+
+You can install the library through the Node Package Manager by running
+`npm install uglify-js-middleware`.
+
+    app.use(uglify.middleware({
+      src : __dirname + '/client',
+      dest: __dirname + '/public',
+    });
+
+If you are not already using `uglify`, the `require()` of this library will
+return the middleware function directly.
+
+
+Configuration
+-------------
+
+The following options are supported:
+
+ * `src`: Source directory of JavaScript files.
+ * `dest`: Destination directory to place uglified files. If omitted, this will
+   default to match `src` and your generated files with be suffixed with
+   `.ugly.js` rather than just `.js`.
+ * `force`: Boolean indicating whether to force uglification to occur on every
+   access.
+ * `mangle`: Boolean indicating whether variable names should be mangled.
+ * `squeeze`: Boolean indicating whether the code should be squeezed.
+ * `uglyext`: Boolean indicating whether to use the `.ugly.js` extension for
+   generated files.
+
+
+
+Developed By
+============
+
+* Jake Wharton - <jakewharton@gmail.com>
+
+
+
+License
+=======
+
+    Copyright 2011 Jake Wharton
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
+
+
+ [1]: https://github.com/mishoo/UglifyJS
+ [2]: http://senchalabs.github.com/connect/
---
+++ uglify-js-middleware.js
@@ -0,0 +1,118 @@
+/**
+ * Module dependencies.
+ */
+
+var uglify = require('uglify-js');
+
+var jsp = uglify.parser,
+    pro = uglify.uglify,
+    fs = require('fs'),
+    url = require('url'),
+    basename = require('path').basename,
+    join = require('path').join,
+    ENOENT;
+
+// COMPAT:
+
+try {
+        ENOENT = require('constants').ENOENT;
+} catch (err) {
+        ENOENT = process.ENOENT;
+}
+
+/**
+ * Return Connect middleware with the given `options`.
+ *
+ * Implementation derived from the Stylus library middleware.
+ *
+ * @param {Object} options
+ * @return {Function}
+ * @api public
+ */
+
+module.exports = uglify.middleware = function(options) {
+        options = options || {};
+
+        //Accept src/dest dir
+        if ('string' === typeof options) {
+                options = { src: options };
+        }
+
+        // Force compilation
+        var force = options.force || false;
+
+        // Variable name mangling
+        var mangle = options.mangle || true;
+
+        // Code squeezing
+        var squeeze = options.squeeze || true;
+
+        // Source dir required
+        var src = options.src;
+        if (!src) throw new Error('uglify.middleware requires "src" directory');
+
+        // Default dest dir to source
+        var dest = options.dest
+          ? options.dest
+          : src;
+
+        // Whether to "ugly" the extension (default true if same dirs, else false)
+        var uglyext = options.uglyext || (src === dest);
+        var uglyregex = uglyext ? /\.ugly\.js$/ : /\.js$/;
+
+        return function(req, res, next) {
+                if ('GET' != req.method && 'HEAD' != req.method) return next();
+
+                var path = url.parse(req.url).pathname;
+                if (uglyregex.test(path)) {
+                        var uglyPath = join(dest, path),
+                            jsPath = join(src, uglyext ? path.replace('.ugly.js', '.js') : path);
+
+                        // Ignore ENOENT to fall through as 404
+                        function error(err) {
+                                next(ENOENT === err.errno ? null : err);
+                        }
+
+                        if (force) return compile();
+
+                        // Compile to uglyPath
+                        function compile() {
+                                fs.readFile(jsPath, 'utf8', function(err, str) {
+                                        if (err) return error(err);
+
+                                        var ast = jsp.parse(str);
+                                        if (mangle) ast = pro.ast_mangle(ast);
+                                        if (squeeze) ast = pro.ast_squeeze(ast);
+                                        var ugly = pro.gen_code(ast);
+                                        fs.writeFile(uglyPath, ugly, 'utf8', function(err) {
+                                                next(err);
+                                        });
+                                });
+                        }
+
+                        fs.stat(jsPath, function(err, jsStats) {
+                                if (err) return error(err);
+                                fs.stat(uglyPath, function(err, uglyStats) {
+                                        if (err) {
+                                                if (ENOENT === err.errno) {
+                                                        // JS has not been uglified, compile it!
+                                                        compile();
+                                                } else {
+                                                        next();
+                                                }
+                                        } else {
+                                                if (jsStats.mtime > uglyStats.mtime) {
+                                                        // Source has changed, compile it!
+                                                        compile();
+                                                } else {
+                                                        // Already compiled
+                                                        next();
+                                                }
+                                        }
+                                });
+                        });
+                } else {
+                        next();
+                }
+        };
+}
