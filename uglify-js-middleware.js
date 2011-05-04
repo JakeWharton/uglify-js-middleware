7033770cbc440f6a382b48415405739edf6920b8
Jake Wharton <jakewharton@gmail.com>
2011-05-03 22:38:29 -0400

    Change back to my prefered style since we're not trying to be accepted into the UglifyJS library directly anymore.

--- uglify-js-middleware.js
+++ uglify-js-middleware.js
@@ -4,20 +4,20 @@
 
 var uglify = require('uglify-js');
 
-var jsp = uglify.parser,
-    pro = uglify.uglify,
-    fs = require('fs'),
-    url = require('url'),
-    basename = require('path').basename,
-    join = require('path').join,
-    ENOENT;
+var jsp = uglify.parser
+  , pro = uglify.uglify
+  , fs = require('fs')
+  , url = require('url')
+  , basename = require('path').basename
+  , join = require('path').join
+  , ENOENT;
 
 // COMPAT:
 
 try {
-        ENOENT = require('constants').ENOENT;
+  ENOENT = require('constants').ENOENT;
 } catch (err) {
-        ENOENT = process.ENOENT;
+  ENOENT = process.ENOENT;
 }
 
 /**
@@ -31,88 +31,88 @@ try {
  */
 
 module.exports = uglify.middleware = function(options) {
-        options = options || {};
+  options = options || {};
 
-        //Accept src/dest dir
-        if ('string' === typeof options) {
-                options = { src: options };
-        }
+  //Accept src/dest dir
+  if ('string' === typeof options) {
+    options = { src: options };
+  }
 
-        // Force compilation
-        var force = options.force || false;
+  // Force compilation
+  var force = options.force || false;
 
-        // Variable name mangling
-        var mangle = options.mangle || true;
+  // Variable name mangling
+  var mangle = options.mangle || true;
 
-        // Code squeezing
-        var squeeze = options.squeeze || true;
+  // Code squeezing
+  var squeeze = options.squeeze || true;
 
-        // Source dir required
-        var src = options.src;
-        if (!src) throw new Error('uglify.middleware requires "src" directory');
+  // Source dir required
+  var src = options.src;
+  if (!src) throw new Error('uglify.middleware requires "src" directory');
 
-        // Default dest dir to source
-        var dest = options.dest
-          ? options.dest
-          : src;
+  // Default dest dir to source
+  var dest = options.dest
+    ? options.dest
+    : src;
 
-        // Whether to "ugly" the extension (default true if same dirs, else false)
-        var uglyext = options.uglyext || (src === dest);
-        var uglyregex = uglyext ? /\.ugly\.js$/ : /\.js$/;
+  // Whether to "ugly" the extension (default true if same dirs, else false)
+  var uglyext = options.uglyext || (src === dest);
+  var uglyregex = uglyext ? /\.ugly\.js$/ : /\.js$/;
 
-        return function(req, res, next) {
-                if ('GET' != req.method && 'HEAD' != req.method) return next();
+  return function(req, res, next) {
+    if ('GET' != req.method && 'HEAD' != req.method) return next();
 
-                var path = url.parse(req.url).pathname;
-                if (uglyregex.test(path)) {
-                        var uglyPath = join(dest, path),
-                            jsPath = join(src, uglyext ? path.replace('.ugly.js', '.js') : path);
+    var path = url.parse(req.url).pathname;
+    if (uglyregex.test(path)) {
+      var uglyPath = join(dest, path)
+        , jsPath = join(src, uglyext ? path.replace('.ugly.js', '.js') : path);
 
-                        // Ignore ENOENT to fall through as 404
-                        function error(err) {
-                                next(ENOENT === err.errno ? null : err);
-                        }
+      // Ignore ENOENT to fall through as 404
+      function error(err) {
+        next(ENOENT === err.errno ? null : err);
+      }
 
-                        if (force) return compile();
+      if (force) return compile();
 
-                        // Compile to uglyPath
-                        function compile() {
-                                fs.readFile(jsPath, 'utf8', function(err, str) {
-                                        if (err) return error(err);
+      // Compile to uglyPath
+      function compile() {
+        fs.readFile(jsPath, 'utf8', function(err, str) {
+          if (err) return error(err);
 
-                                        var ast = jsp.parse(str);
-                                        if (mangle) ast = pro.ast_mangle(ast);
-                                        if (squeeze) ast = pro.ast_squeeze(ast);
-                                        var ugly = pro.gen_code(ast);
-                                        fs.writeFile(uglyPath, ugly, 'utf8', function(err) {
-                                                next(err);
-                                        });
-                                });
-                        }
+          var ast = jsp.parse(str);
+          if (mangle) ast = pro.ast_mangle(ast);
+          if (squeeze) ast = pro.ast_squeeze(ast);
+          var ugly = pro.gen_code(ast);
+          fs.writeFile(uglyPath, ugly, 'utf8', function(err) {
+            next(err);
+          });
+        });
+      }
 
-                        fs.stat(jsPath, function(err, jsStats) {
-                                if (err) return error(err);
-                                fs.stat(uglyPath, function(err, uglyStats) {
-                                        if (err) {
-                                                if (ENOENT === err.errno) {
-                                                        // JS has not been uglified, compile it!
-                                                        compile();
-                                                } else {
-                                                        next();
-                                                }
-                                        } else {
-                                                if (jsStats.mtime > uglyStats.mtime) {
-                                                        // Source has changed, compile it!
-                                                        compile();
-                                                } else {
-                                                        // Already compiled
-                                                        next();
-                                                }
-                                        }
-                                });
-                        });
-                } else {
-                        next();
-                }
-        };
+      fs.stat(jsPath, function(err, jsStats) {
+        if (err) return error(err);
+        fs.stat(uglyPath, function(err, uglyStats) {
+          if (err) {
+            if (ENOENT === err.errno) {
+              // JS has not been uglified, compile it!
+              compile();
+            } else {
+              next();
+            }
+          } else {
+            if (jsStats.mtime > uglyStats.mtime) {
+              // Source has changed, compile it!
+              compile();
+            } else {
+              // Already compiled
+              next();
+            }
+          }
+        });
+      });
+    } else {
+      next();
+    }
+  };
 }
